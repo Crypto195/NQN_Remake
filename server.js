0acc8b9121f57c8ae958380bdbdcf267b018965d
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-02-11 19:28:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,81 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+const token = ("BOT-TOKEN-HERE") //put your bot's token here
+let contentStart;
+let contentEnd;
+
+client.on("ready", () => {
+    console.log("Readt");
+})
+
+client.on("message", async (message) => {
+    if(message.author.bot) return;
+    let substringArray = get_substrings_between(message.content, ":", ":");
+    let msg = message.content;
+    if(!substringArray.length) return;
+
+    substringArray.forEach(m => {
+        let emoji = client.emojis.cache.find(x => x.name === m);
+        var replace = `:${m}:`;
+        var rexreplace = new RegExp(replace, 'g');
+
+        if(emoji && !msg.split(" ").find(x => x === emoji.toString()) && !msg.includes(`<a${replace}${emoji.id}>`)) msg = msg.replace(rexreplace, emoji.toString());
+    })
+    
+
+    if(msg === message.content) return;
+
+    let webhook = await message.channel.fetchWebhooks();
+    webhook = webhook.find(x => x.name === "Zeyron");
+
+    if(!webhook) {
+        webhook = await message.channel.createWebhook(`Zeyron`, {
+            avatar: client.user.displayAvatarURL({dynamic: true})
+        });
+    }
+
+    await webhook.edit({
+        name: message.member.nickname ? message.member.nickname : message.author.username,
+        avatar: message.author.displayAvatarURL({dynamic: true})
+    })
+
+    message.delete().catch(m => {})
+
+    webhook.send(msg).catch( m => {});
+
+    await webhook.edit({
+        name: `Zeyron`,
+        avatar: client.user.displayAvatarURL({dynamic:true})
+    })
+
+ 
+})
+
+
+
+client.login(token);
+
+function get_substrings_between(str, startDelimiter, endDelimiter) {
+    var contents = [];
+    var startDelimiterLength = startDelimiter.length;
+    var endDelimiterLength = endDelimiter.length;
+    var startFrom = contentStart = contentEnd = 0;
+  
+    while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
+      contentStart += startDelimiterLength;
+      contentEnd = strpos(str, endDelimiter, contentStart);
+      if (false === contentEnd) {
+        break;
+      }
+      contents.push(str.substr(contentStart, contentEnd - contentStart));
+      startFrom = contentEnd + endDelimiterLength;
+    }
+  
+    return contents;
+  }
+  
+  
+  function strpos(haystack, needle, offset) {
+    var i = (haystack + '').indexOf(needle, (offset || 0));
+    return i === -1 ? false : i;
+  }
