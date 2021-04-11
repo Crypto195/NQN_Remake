2087a9af6e2e2ff493fc2758af80a36858457452
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-11 09:33:15 +0530

    invite

---
+++ commands/invite.js
@@ -0,0 +1,21 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  category: "info",
+  description: "Invite link",
+  run: async (client, message, args) => {
+    
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Invite me!`)
+    .setDescription(`[Invite Link](https://discord.com/api/oauth2/authorize?client_id=+`Client.user.id`+permissions=8&scope=bot) OR [Github Link](https://github.com/Crypto195/NQN_Remake)`)
+    .setColor("RANDOM")
+    .setFooter(`Bot by Crypto`)
+    .setTimestamp(message.timestamp = Date.now())
+    
+    message.channel.send(embed)
+    
+  
+  }
+}
