409ccb2196f1b1dab84b628a878059fc32346d08
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-11 09:36:18 +0530

    Update howtodo.js

--- commands/howtodo.js
+++ commands/howtodo.js
@@ -1,1 +1,29 @@
+const discord = require("discord.js");
 
+module.exports = {
+  name: "invite",
+  category: "info",
+  description: "Invite link",
+  run: async (client, message, args) => {
+    
+    let embed1 = new discord.MessageEmbed()
+    .setTitle(`Type the image`)
+    .setColor("RANDOM")
+    .setImage("https://raw.githubusercontent.com/Crypto195/NQN_Remake/main/before.PNG")
+    .setFooter(`Bot by Crypto`)
+    .setTimestamp(message.timestamp = Date.now())
+    
+    message.channel.send(embed1);
+    
+     let embed2 = new discord.MessageEmbed()
+    .setTitle(`Send and Voila`)
+    .setColor("RANDOM")
+    .setImage("https://raw.githubusercontent.com/Crypto195/NQN_Remake/main/after.PNG")
+    .setFooter(`Bot by Crypto`)
+    .setTimestamp(message.timestamp = Date.now())
+    
+    message.channel.send(embed2)
+    
+  
+  }
+}
