2448c5c7fd46bbe23f9bc9dc0ba2acfaa3497784
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-11 09:46:21 +0530

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,27 +1,20 @@
 const { readdirSync } = require("fs");
 
-const ascii = require("ascii-table");
-
-let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
-
 module.exports = (client) => {
     readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands`).filter(file => file.endsWith(".js"));
-    
+        const commands = readdirSync(`./commands/`).filter(file => file.endsWith(".js"));
         for (let file of commands) {
             let pull = require(`../commands/${file}`);
     
             if (pull.name) {
                 client.commands.set(pull.name, pull);
-                table.addRow(file, '✅');
+                console.log(`[${pull.name.toUpperCase()}]: loaded!`)
             } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+              console.log(`[${file.toUpperCase()}]: Error`)
                 continue;
             }
     
             if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
-    console.log(table.toString());
-}
+    console.log("Commands Loaded!")
