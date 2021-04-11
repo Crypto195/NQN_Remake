63117bc0753c886ac849577e72d5bf07dde420cf
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-11 09:15:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,81 +1,60 @@
 const discord = require("discord.js");
 const client = new discord.Client();
-const token = ("BOT-TOKEN-HERE") //put your bot's token here
-let contentStart;
-let contentEnd;
+const { token } = require("./config.json")
 
 client.on("ready", () => {
-    console.log("Ready");
+  console.log("Bot by Crypto");
+  client.user.setActivity("with emojis");
 })
 
 client.on("message", async (message) => {
-    if(message.author.bot) return;
-    let substringArray = get_substrings_between(message.content, ":", ":");
-    let msg = message.content;
-    if(!substringArray.length) return;
+  if (message.author.bot) return;
+  let msg = message.content;
 
-    substringArray.forEach(m => {
-        let emoji = client.emojis.cache.find(x => x.name === m);
-        var replace = `:${m}:`;
-        var rexreplace = new RegExp(replace, 'g');
+  let emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g)
+  if (!emojis) return;
+  emojis.forEach(m => {
+    let emoji = client.emojis.cache.find(x => x.name === m)
+    if (!emoji) return;
+    let temp = emoji.toString()
+    if (new RegExp(temp, "g").test(msg)) msg = msg.replace(new RegExp(temp, "g"), emoji.toString())
+    else msg = msg.replace(new RegExp(":" + m + ":", "g"), emoji.toString());
+  })
 
-        if(emoji && !msg.split(" ").find(x => x === emoji.toString()) && !msg.includes(`<a${replace}${emoji.id}>`)) msg = msg.replace(rexreplace, emoji.toString());
-    })
-    
+  if (msg === message.content) return;
 
-    if(msg === message.content) return;
+  let webhook = await message.channel.fetchWebhooks();
+  let number = randomNumber(1, 2);
+  webhook = webhook.find(x => x.name === "NQN" + number);
 
-    let webhook = await message.channel.fetchWebhooks();
-    webhook = webhook.find(x => x.name === "Zeyron");
+  if (!webhook) {
+    webhook = await message.channel.createWebhook(`NQN` + number, {
+      avatar: client.user.displayAvatarURL({ dynamic: true })
+    });
+  }
 
-    if(!webhook) {
-        webhook = await message.channel.createWebhook(`Zeyron`, {
-            avatar: client.user.displayAvatarURL({dynamic: true})
-        });
-    }
+  await webhook.edit({
+    name: message.member.nickname ? message.member.nickname : message.author.username,
+    avatar: message.author.displayAvatarURL({ dynamic: true })
+  })
 
-    await webhook.edit({
-        name: message.member.nickname ? message.member.nickname : message.author.username,
-        avatar: message.author.displayAvatarURL({dynamic: true})
-    })
+  message.delete().catch(err => { })
+  webhook.send(msg).catch(err => { })
 
-    message.delete().catch(m => {})
+  await webhook.edit({
+    name: `NQN` + number,
+    avatar: client.user.displayAvatarURL({ dynamic: true })
+  })
 
-    webhook.send(msg).catch( m => {});
 
-    await webhook.edit({
-        name: `Zeyron`,
-        avatar: client.user.displayAvatarURL({dynamic:true})
-    })
-
- 
 })
 
 
 
 client.login(token);
 
-function get_substrings_between(str, startDelimiter, endDelimiter) {
-    var contents = [];
-    var startDelimiterLength = startDelimiter.length;
-    var endDelimiterLength = endDelimiter.length;
-    var startFrom = contentStart = contentEnd = 0;
-  
-    while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
-      contentStart += startDelimiterLength;
-      contentEnd = strpos(str, endDelimiter, contentStart);
-      if (false === contentEnd) {
-        break;
-      }
-      contents.push(str.substr(contentStart, contentEnd - contentStart));
-      startFrom = contentEnd + endDelimiterLength;
-    }
-  
-    return contents;
-  }
-  
-  
-  function strpos(haystack, needle, offset) {
-    var i = (haystack + '').indexOf(needle, (offset || 0));
-    return i === -1 ? false : i;
-  }
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
